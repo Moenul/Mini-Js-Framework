f7e4943d7cea1d2f89d22d4bf62ff6036f2e1a42
Moenul <moenul22@gmail.com>
2025-03-10 15:25:18 +0600

    Binding Data to UI - Connect Reactive data to Dom

--- framework.js
+++ framework.js
@@ -21,3 +21,28 @@ class Reactive {
         this.subscribers.forEach((callback) => callback(this._data));
     }
 }
+
+class MiniFramework {
+    constructor(options) {
+        this.$el = document.querySelector(options);
+        this.data = {};
+
+        for (let key in options.data) {
+            this.data[key] = new Reactive(options.data[key]);
+        }
+
+        this.render;
+    }
+
+    render() {
+        this.$el.innerHTML = this.$el.innerHTML.replace(
+            /\{\{(.*?)\}\}/g,
+            (_, key) => this.data[key.trim()].value
+        );
+
+        // Subscribe to changes so UI updates automatically
+        for (let key in this.data) {
+            this.data[key].subscribe(() => this.render());
+        }
+    }
+}
